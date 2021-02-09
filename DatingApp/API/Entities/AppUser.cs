138488e38adc83bf34cacfae964908f5506fe562
Ganesha Nageshappa <Ganesha.nageshappa@amadeus.com>
2021-02-09 15:11:23 +0530

    API Skeleton.

---
+++ DatingApp/API/Entities/AppUser.cs
@@ -0,0 +1,19 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set;}
+        // public string FirstName { get; set; }
+        // public string LastName { get; set; }
+
+        // private int phNumber;
+        // public int PhoneNumber 
+        // {
+        //     get {return phNumber; }
+        //     set { phNumber = value; }
+        // }
+    }
+
+}
